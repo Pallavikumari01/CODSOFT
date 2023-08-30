99f7fdfd51ba6ca390b889492da1d8739720ba46
Pallavi Kumari <115500832+Pallavikumari01@users.noreply.github.com>
2023-08-31 00:12:03 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,48 @@
+let menuIcons= document.querySelector('#menu-icon');
+let navbar= document.querySelector('.navbar');
+menuIcons.onclick= ()=> {
+    menuIcons.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+    
+    
+
+};
+
+let sections= document.querySelectorAll('section');
+let navbarLinks= document.querySelectorAll('header navbar a');
+window.onscroll=()=>{
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop- 150;
+        let height= sec.offsetHeight;
+        let id= sec.getAttribute('id');
+        if(top >= offset && top< offset + height ){
+            navbarLinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*= ' + id +']' ).classList.add('active');
+
+            });
+                
+        };
+
+    });
+    let header= document.querySelector('header');
+    
+    header.classList.toggle('sticky',window.scrollY > 100);
+    
+    menuIcons.classList.remove('bx-x');
+
+    navbar.classList.remove('active');
+    
+    
+    
+
+};
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+ });
+ ScrollReveal().reveal('.home-content, .heading', { origin : 'top' });
+ 
